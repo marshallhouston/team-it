e62e8240e4472cc254bbddddb36957dcb278ff1f
Marshall Houston <marshall.houston@gmail.com>
2018-02-15 17:13:18 -0700

    Update TeamCard with Link to team page

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const TeamCard = ({ team } ) => {
   return (
-    <div className="team-card">
-      <h2 className="team-name"> { team.name }</h2>
-      <h2 className="team-phone"> { team.phone }</h2>
-    </div>
+      <div className="team-card">
+        <Link to={`/teams/${team.id}`}>
+        <h2 className="team-name"> { team.name }</h2>
+        <h2 className="team-phone"> { team.phone }</h2>
+        </Link>
+      </div>
   )
 }
 
