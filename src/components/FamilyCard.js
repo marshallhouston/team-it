f810e95bb35d712039cd21eb6d321b2f9fe8628a
Marshall Houston <marshall.houston@gmail.com>
2018-02-18 12:27:26 -0700

    add structure for family card

--- src/components/FamilyCard.js
+++ src/components/FamilyCard.js
@@ -3,9 +3,15 @@ import React from 'react'
 const FamilyCard = ({ family }) => {
   return (
     <div className="family-card">
-      <p className="family-name">{ family.name }</p>
-      <p className="family-phone">{ family.phone }</p>
-      <p className="family-email">{ family.email }</p>
+      <div className="family-details-container">
+        <p className="family-name">{ family.name }</p>
+      </div>
+      <div className="family-details-container">
+        <p className="family-phone">{ family.phone }</p>
+      </div>
+      <div className="family-details-container">
+        <p className="family-email">{ family.email }</p>
+      </div>
     </div>
   )
 }
