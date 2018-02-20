cc3c4ab2d206df3a25d6bd687c9c7408ccc22f14
Marshall Houston <marshall.houston@gmail.com>
2018-02-20 11:18:05 -0700

    Call getTeamDetails when Team component mounts, update state.

--- src/components/Team.js
+++ src/components/Team.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import TeamInfo from './TeamInfo'
 import FollowerList from './FollowerList'
 import NewFollowerForm from './NewFollowerForm'
+import { getTeamDetails } from '../utils/requests'
 
 class Team extends Component {
   constructor(props) {
@@ -11,6 +12,16 @@ class Team extends Component {
       followers: [{id: 2, name: "Amy Raymond", phone: "555-555-5555", email: "amy@parent.com"}, {id: 3, name: "Greg McGee", phone: "555-555-5555", email: "greg@parent.com"}],
       teamId: parseInt(props.match.params.id, 10)
     }
+  }
+
+  componentDidMount() {
+  const teamId = this.state.teamId
+  getTeamDetails(teamId)
+    .then(teamDetails => this.setState({
+      teamInfo: teamDetails.teamInfo,
+      followers: teamDetails.followers
+    }))
+    .catch(error => console.error({ error }))
   }
 
   updateFollowers = (name, phone, email) => {
