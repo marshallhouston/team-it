dcfc1034152d9b1c79ad6ac83761241fa901f31a
Marshall Houston <marshall.houston@gmail.com>
2018-02-20 09:35:44 -0700

    Call getTeams after component mounts, remove hardcoded data

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import TeamList from './TeamList'
 import Team from './Team'
+import { getTeams } from '../utils/requests'
 
-// team info hardcoded right now, will hit API to gather all team info
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      teams: [{id: 1, name: "Westwood Boy's Soccer", phone: "555-555-5555"}, {id: 15, name: "Lynx Baseball", phone: "503-503-5033"}]
+      teams: []
     }
+  }
+
+  componentDidMount() {
+  getTeams()
+    .then((teams) => this.setState({ teams }))
+    .catch((error) => console.error({ error }))
   }
 
   render() {
