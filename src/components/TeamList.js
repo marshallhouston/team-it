ec3541a8696b15224cbe22669e17773707dc1b74
Marshall Houston <marshall.houston@gmail.com>
2018-02-20 19:59:45 -0700

    Change TeamList to stateful component, render NewTeamForm, getTeams

--- src/components/TeamList.js
+++ src/components/TeamList.js
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react'
 import TeamCard from './TeamCard'
+import { getTeams } from '../utils/requests'
+import NewTeamForm from './NewTeamForm'
 
 const createTeamCards = (teams) => {
   return teams.map(team => {
     return (
       <TeamCard
-        key={ team.id }
+        key={ team.name + team.id }
         team={ team }
       />
     )
   })
 }
 
-const TeamList = ({ teams }) => {
-  return (
-    <div className="teams-list">
-      <h2>Your Teams</h2>
-      { createTeamCards(teams)}
-    </div>
-  )
+class TeamList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      teams: []
+    }
+  }
+
+  componentDidMount() {
+    getTeams()
+      .then((teams) => this.setState({ teams }))
+      .catch((error) => console.error({ error }))
+  }
+
+  updateTeams = (name, phone) => {
+    this.setState({ teams: [...this.state.teams, { name, phone }] })
+  }
+
+  render() {
+    return (
+      <div className="teams-list">
+        <h2>Your Teams</h2>
+        { createTeamCards(this.state.teams) }
+        <NewTeamForm
+          updateTeams={ this.updateTeams }
+        />
+      </div>
+    )
+  }
 }
 
 export default TeamList
