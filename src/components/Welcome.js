80aa4ff9de4eef4a5b4301e3dc8ae27aca01c9da
Marshall Houston <marshall.houston@gmail.com>
2018-02-25 12:47:14 -0700

    Add initial "how to section" on welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,6 +4,10 @@ const Welcome = () => {
   return (
     <div className="welcome">
       <h1> Welcome to Team It! </h1>
+      <p> Send a text message to 1-205-639-0334 with your team code, followed by a space, and then your message.</p>
+      <p> All the followers will receive your message! </p>
+      <p> Example: "933 Practice has been changed to 3:00 pm. See you there!"</p>
+      <p> This will send a text to all the followers of team 933!</p>
     </div>
   )
 }
